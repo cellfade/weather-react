64caff955d7fda14105390254db433b22a634c2d
Andrew Miller <cellfade@gmail.com>
2019-05-08 15:48:56 -0600

    new footer and icons.js

---
+++ src/Layout/footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import 'typeface-roboto';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function Footer(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Footer);
